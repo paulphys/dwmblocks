7d1f625036b098de3ae77c08e8ba1df3b46711f1
paul <paul@physics.sh>
2021-12-07 23:15:46 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
@@ -5,18 +6,18 @@ static const Block blocks[] = {
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-price eth Ethereum 💎 ",	9000,	23},
-	{"",	"sb-price btc Bitcoin 💰 ",				9000,	21},
-	{"",	"sb-price sol Solana 🟣 ", 	9000,	24},
-	{"",	"sb-memory",	10,	14},
-	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-price eth Ethereum 🟢",	9000,	23},
+	{"",	"sb-price btc Bitcoin 🟡 ",				9000,	21},
+	{"",	"sb-price sol Solana 🟣", 9000,24},
+/*	{"",	"sb-memory",	10,	14}, /*
+/*	{"",	"sb-cpu",		10,	18}, */
 /*	{"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-forecast",	5000,	5},
 	{"",	"sb-mailbox",	180,	12},
 /*	{"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
-  {"",  "sb-internet",5,4},
+  	{"",  "sb-internet",	5,	4},
 	{"",	"sb-clock",	60,	1},
 };
 
@@ -25,5 +26,4 @@ static char *delim = " ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
-
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
